b1eb4b83154fb032603e4f6969f7f86446554c2a
kalazus <kalazus@mail.ru>
2023-02-18 10:39:14 +0300

    add seed utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,10 @@
 import os
+import random
 from typing import Union
 
 import nibabel as nib
 import numpy as np
+import torch
 
 
 def nii_to_np(filepath: Union[str, "os.PathLike"]) -> "np.ndarray":
@@ -22,3 +24,23 @@ def nii_to_np(filepath: Union[str, "os.PathLike"]) -> "np.ndarray":
     array = ct_scan.get_fdata()
     array = np.rot90(array)  # for better visualization
     return array
+
+
+def seed_all(seed: int):
+    r"""Seeds randomisation for torch, numpy and randoms
+
+    Parameters
+    ----------
+    seed : unsigned int
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+
+def seed_worker(worker_id):
+    r"""Seeds current worker"""
+    seed = torch.utils.data.get_worker_info().seed % (2 ^ 32)
+    seed_all(seed)
