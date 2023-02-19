7ea63d158c4eff2c518021f538f7d704b02ad31b
kalazus <kalazus@mail.ru>
2023-02-19 19:46:35 +0300

    add requirements.txt

--- setup.py
+++ setup.py
@@ -1,4 +1,17 @@
+import os
+
 from setuptools import find_packages, setup
+
+_PATH_ROOT = os.path.realpath(os.path.dirname(__file__))
+_PATH_REQUIREMENTS = os.path.join(_PATH_ROOT, "requirements.txt")
+
+
+def _load_requirements(file_path: str = "requirements.txt"):
+    """Load requirements from a file."""
+    with open(file_path) as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
 
 if __name__ == "__main__":
     setup(
@@ -7,4 +20,6 @@ if __name__ == "__main__":
         description="Covid19 segmentation on ct scans",
         packages=find_packages("src"),
         package_dir={"": "src"},
+        python_requires=">=3.8",
+        install_requires=_load_requirements(_PATH_REQUIREMENTS),
     )
