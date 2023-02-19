0fe7e3e195d497d180746cce74f913cd678cce2f
kalazus <kalazus@mail.ru>
2023-02-19 13:47:46 +0300

    forgot to remove dir check from train script

--- scripts/train.py
+++ scripts/train.py
@@ -1,9 +1,6 @@
 from ctcovseg import Config, train
 
 if __name__ == "__main__":
-    import os
-
-    print(os.getcwd())
     config = Config()
     metrics = train(config, "input/pngs")
     for metric, value in metrics.items():
